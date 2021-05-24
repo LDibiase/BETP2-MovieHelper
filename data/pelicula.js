05f78bd31b3b78be31d7be0460973d5247d19710
Ornella Stirparo <ornellaluz@hotmail.com>
2021-05-24 20:20:24 -0300

    arreglo

--- data/pelicula.js
+++ data/pelicula.js
@@ -1,18 +1,18 @@
 
 //intento fallido xd
 
-import axios from 'axios';
+//import axios from 'axios';
 
-const fs = require('fs').promises;
+//const fs = require('fs').promises;
 
 
 
-async function getPeliculas(){
+//async function getPeliculas(){
     // const peliculas =  await fs.readFile(path, 'utf-8')
    
     // return JSON.parse(peliculas)
-    return axios.request('https://streaming-availability.p.rapidapi.com/search/basic');
-}
+  //  return axios.request('https://streaming-availability.p.rapidapi.com/search/basic');
+//}
 
 
 // async function getPelicula(id){
@@ -21,4 +21,4 @@ async function getPeliculas(){
 //     return peliculas.find(p => p.id == id)
 // }
 
-export default getPeliculas
+//export default getPeliculas
--- routes/peliculas.js
+++ routes/peliculas.js
@@ -1,15 +1,19 @@
-
-//intento fallido xd
-
 var express = require('express');
+const { response } = require('../app.js');
 var router = express.Router();
 const dataPeliculas = require('../data/pelicula.js')
 
+//const fetch = require('')
 /* GET users listing. */
 router.get('/', function(req, res) {
-   const peliculas = await dataPeliculas.getPeliculas();
+   //const peliculas = await dataPeliculas.getPeliculas();
+//  return peliculas 
+//const peliculas = await fetch('https://streaming-availability.p.rapidapi.com/genres');
+//console.log(peliculas)
+//const pelis = await peliculas.json();
+//res.json(pelis)
+res.send('la peli anda');
 
-   return peliculas 
 });
 
 module.exports = router;
