5026227d5c4f50345550b243d52bdd369dff31b1
im-agarcia <im.agarcia@outlook.com>
2021-06-10 23:43:52 -0300

    env y routes generos

--- routes/genres.js
+++ routes/genres.js
@@ -1,21 +1,35 @@
 const axios = require("axios").default;
-require('dotenv').config(); //para las variables de entorno
+const dotenv = require('dotenv').config(); //para las variables de entorno
+
+console.log(dotenv)
 
 const options = {
   method: 'GET',
   url: 'https://streaming-availability.p.rapidapi.com/genres',
   headers: {
 
+/* 'x-rapidapi-key': '2e201709d3mshcc8b942e035c08ap1c5bbfjsn9dba6c19c6b6',
+'x-rapidapi-host': 'streaming-availability.p.rapidapi.com' */
     //en heroku
     'x-rapidapi-key': process.env.X_RAPIDAPI_KEY,
-    'x-rapidapi-host': process.env.X-RAPIDAPI-HOST
+    'x-rapidapi-host': process.env.X_RAPIDAPI_HOST
   }
 };
 
+/* console.log(process.env); */
+
+/* axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+ */
+/* 
 function codeGenre(codigo){
     axios.request(options).then(function (response) {
 	    return response.data.codigo
 }).catch(function (error) {
 	console.error(error);
 });
-};
+}; */
+
