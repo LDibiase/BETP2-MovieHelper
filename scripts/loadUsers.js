6791e16283872eeed4a84fba661af33f502c3c03
Ornella Stirparo <ornellaluz@hotmail.com>
2021-06-19 12:17:13 -0300

    intento fallido de coneccion a la base para usuarios

--- scripts/loadUsers.js
+++ scripts/loadUsers.js
@@ -1,7 +1,11 @@
 db = db.getSiblingDB('moviehelper');
 db.usuarios.drop();
-db.usuarios.insertMany([
-    {
-        "_id": 0
-    }
-]);
+db.usuarios.insertMany([{
+    "_id": 123,
+    "name": "orne",
+    "email": "cualquiera@gmail.com"
+}])
+//para cargar manualmente (que no sirve de nada porque quiero cargar usuarios), entrar a scrips: 
+// ejecutar: mongo "mongodb+srv://cluster0.hegwv.mongodb.net/moviehelper" --username moviehelper
+//contrasenia: moviehelper123
+// despues:  load('loadusers.js') => tiene que devolver true
